2b5a4a68db9981bec5887039235d7c1622e3990d
Moshe Gelberg <glbrgm@gmail.com>
2019-02-24 23:27:05 +0200

    Added console styling.

--- commands/TaskController.php
+++ commands/TaskController.php
@@ -6,6 +6,8 @@ namespace app\commands;
 use app\models\Mailer;
 use Yii;
 use yii\console\Controller;
+use yii\console\ExitCode;
+use yii\helpers\Console;
 
 /**
  * Class TaskController contains task utilities.
@@ -27,7 +29,13 @@ class TaskController extends Controller
             $message = "Dear {$task['name']}, the task {$task['title']} due date expires in less than one day 
                 on {$task['due_date']}. \nPlease, follow to {$taskReference} and finish it.\n";
 
-            Mailer::notify($task['email'], $subject, $message);
+            if (!Mailer::notify($task['email'], $subject, $message)) {
+                $this->stdout("Error occurred!\n", Console::FG_RED, Console::BOLD);
+                return ExitCode::UNSPECIFIED_ERROR;
+            }
         }
+
+        $this->stdout("Notifications successfully sent.\n", Console::FG_GREEN, Console::BOLD);
+        return ExitCode::OK;
     }
 }
